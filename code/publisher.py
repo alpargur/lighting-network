fcaa2ab86db658c957b04460e2e2f270b3817249
alpargur <alpar.guer@vaillant-group.com>
2023-11-19 23:26:48 +0100

    refactor the midi publisher

--- code/publisher.py
+++ code/publisher.py
@@ -2,45 +2,56 @@ import rtmidi
 import paho.mqtt.client as mqtt
 
 ## configuration variables
-network_ip = "172.20.10.14"
-network_port = 1884
-keep_alive = 60
-topic = "midi-note.received"
+NETWORK_IP = "172.20.10.14"
+NETWORK_PORT = 1884
+KEEP_ALIVE = 60
+DESTINATION_TOPIC = "midi-note.received"
+MIDI_PORT = 0 # virtual bus midi port
 
-## establish mqtt connection
-client = mqtt.Client()
-client.connect(network_ip, network_port, keep_alive)
+def midi_publisher(midi_in):
+    ## get the available midi input ports
+    midi_ports = range(midi_in.getPortCount())
+    
+    ## establish mqtt connection
+    # client = mqtt.Client()
+    # client.connect(NETWORK_IP, NETWORK_PORT, KEEP_ALIVE)
 
-## create a midi-in channel
-midi_in = rtmidi.RtMidiIn()
+    if not midi_ports:
+        print("No MIDI input port's available.")
+        return
 
-def publish_message(midi):
+    midi_in.openPort(MIDI_PORT)
+    print("Listening to MIDI input on port: ", midi_in.getPortName(MIDI_PORT))
+
+    ## set the callback function to handle incoming MIDI messages
+    midi_in.setCallback(midi_handler)
+
+    try:
+        input("Press Enter to exit...\n")
+    except KeyboardInterrupt:
+        pass
+
+    # Clean up
+    midi_in.closePort()
+    midi_in.cancelCallback()
+    # client.disconnect()
+
+def midi_handler(midi):
     if midi.isNoteOn():
-        print('ON:\t', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
-        # publish message
-        client.publish(topic, "hello world")
         print('MIDI NOTE PUBLISHED')
+        print('ON:\t', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity(), '\n')
+        # publish message
+        # TODO: parse and serialize midi messages
+        # client.publish(DESTINATION_TOPIC, "hello world")
 
     elif midi.isNoteOff():
         print('OFF:\t', midi.getMidiNoteName(midi.getNoteNumber()))
 
     elif midi.isController():
-        print('MIDI Controller Number:\t', midi.getControllerNumber(), '\tMIDI Controller Value:\t' midi.getControllerValue())
+        print('MIDI Controller Number:\t' + midi.getControllerNumber() + '\tMIDI Controller Value:\t' + midi.getControllerValue())
 
 
-midi_ports = range(midi_in.getPortCount())
-if midi_ports:
-    for midi_port in midi_ports:
-        print(midi_in.getPortName(midi_port))
-    print("Opening port 0!")
-    midi_in.openPort(0)
-
-    while True:
-        message = midi_in.getMessage(2500) # some timeout in ms
-        if message:
-            print_message(message)
-else:
-    print('No available MIDI input port found. Check the port.')
-
-client.disconnect()
+if __name__ == "__main__":
+    midi_in = rtmidi.RtMidiIn()
+    midi_publisher(midi_in)
     
