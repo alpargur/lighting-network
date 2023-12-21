f45bcc708d74d726a6225a2e03192d21dc1f11f4
alpargur <alpar.guer@vaillant-group.com>
2023-12-21 18:27:12 +0100

    add timeout

--- code/subscriber.py
+++ code/subscriber.py
@@ -40,13 +40,24 @@ def on_message(client, data, message):
     midi_key = message['keyNumber']
 
     if midi_key == drum_kit['bass-drum'] :
-        led_strip[5] = ((255, 0, 0))
+        led_strip[:5].fill((255, 0, 0))
+        time.sleep(0.18)
+        led_strip[:5].fill((0, 0, 0))
+
     elif midi_key == drum_kit['clap'] :
-        led_strip[5:11] = ((255,255, 0))
+        led_strip[5:11] = ((255, 255, 0))
+        time.sleep(0.18)
+        led_strip[17:23].fill((0, 0, 0))
+
     elif midi_key == drum_kit['crash'] :
         led_strip[11:17] = ((255, 0, 255))
+        time.sleep(0.18)
+        led_strip[17:23].fill((0, 0, 0))
+
     elif midi_key == drum_kit['hi-tom'] :
         led_strip[17:23] = ((0, 0, 255))
+        time.sleep(0.18)
+        led_strip[17:23].fill((0, 0, 0))
     
     # print()
     # led_strip.fill((0, 6, 10))
