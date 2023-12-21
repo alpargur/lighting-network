94295ea9dcb6d5479c4c81530541782bcea51517
alpargur <alpar.guer@vaillant-group.com>
2023-12-21 18:34:11 +0100

    add

--- code/subscriber.py
+++ code/subscriber.py
@@ -20,7 +20,7 @@ BRIGHTNESS = 20
 QOS = 1 # Quality of Service level, possible values => 0, 1, 2
 
 led_strip = neopixel.NeoPixel(GIO_PIN, LED_COUNT, brightness=BRIGHTNESS)
-
+ 
 # midi key and led mappings
 drum_kit = {
     'ride': 51, 'crash': 49, 'hi-tom': 47, 'open-hi-hat': 46, 'mid-tom': 45, 'low-tom': 44, 'snare-drum': 43, 
@@ -40,24 +40,24 @@ def on_message(client, data, message):
     midi_key = message['keyNumber']
 
     if midi_key == drum_kit['bass-drum'] :
-        led_strip[:5].fill((255, 0, 0))
-        time.sleep(0.18)
-        led_strip[:5].fill((0, 0, 0))
+        for i in range(5):
+            led_strip[i] = (255, 0, 0)  # Set the color for each LED
+        led_strip.show()
 
-    elif midi_key == drum_kit['clap'] :
-        led_strip[5:11] = ((255, 255, 0))
-        time.sleep(0.18)
-        led_strip[17:23].fill((0, 0, 0))
+    # elif midi_key == drum_kit['clap'] :
+    #     led_strip[5:11] = ((255, 255, 0))
+    #     time.sleep(0.18)
+    #     led_strip[17:23].fill((0, 0, 0))
 
-    elif midi_key == drum_kit['crash'] :
-        led_strip[11:17] = ((255, 0, 255))
-        time.sleep(0.18)
-        led_strip[17:23].fill((0, 0, 0))
+    # elif midi_key == drum_kit['crash'] :
+    #     led_strip[11:17] = ((255, 0, 255))
+    #     time.sleep(0.18)
+    #     led_strip[17:23].fill((0, 0, 0))
 
-    elif midi_key == drum_kit['hi-tom'] :
-        led_strip[17:23] = ((0, 0, 255))
-        time.sleep(0.18)
-        led_strip[17:23].fill((0, 0, 0))
+    # elif midi_key == drum_kit['hi-tom'] :
+    #     led_strip[17:23] = ((0, 0, 255))
+    #     time.sleep(0.18)
+    #     led_strip[17:23].fill((0, 0, 0))
     
     # print()
     # led_strip.fill((0, 6, 10))
